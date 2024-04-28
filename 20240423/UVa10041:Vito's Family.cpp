1be1e94f9dd6ea610f6f2f40f6a20489d41cc0bb
MnO2 <57294562+ChMno2@users.noreply.github.com>
2024-04-28 13:33:48 +0800

    Create UVa10041:Vito's Family.cpp

---
+++ 20240423/UVa10041:Vito's Family.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n;
+	cin>>n;
+	for(int i=0;i<n;i++)
+	{
+		int d;
+		cin>>d;
+		vector<int> s(d);
+		for(int j=0;j<d;j++)
+		{
+			cin>>s[j];
+		}
+		sort(s.begin(),s.end());
+		int sum=0;
+		for(auto&r:s)
+		{
+			sum+=abs(r-s[d/2]);
+		}
+		cout<<sum<<endl;
+		
+		
+	}
+}
